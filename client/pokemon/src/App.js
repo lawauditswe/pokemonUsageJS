5feafe7fd384e144579c99fb77e30e1bb8a33a88
TX Ho <tingxianho@gmail.com>
2022-05-20 19:44:08 +0100

    Form created

--- client/pokemon/src/App.js
+++ client/pokemon/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Form from './Form';
 
 function App() {
 	return (
 		<div className='App'>
-			<h1>Hello World</h1>
+			<h1>PokePast Converter</h1>
+			<Form />
 		</div>
 	);
 }
